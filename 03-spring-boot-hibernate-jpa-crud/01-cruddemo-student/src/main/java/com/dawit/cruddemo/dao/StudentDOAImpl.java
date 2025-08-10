e58ccd6f05577b585fabecd8b4f087c0d4a82317
Dawit Alem <dawitt.alem@gmail.com>
2025-08-10 06:35:26 -0400

    Save a java object with JPA

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/dawit/cruddemo/dao/StudentDOAImpl.java
@@ -0,0 +1,30 @@
+package com.dawit.cruddemo.dao;
+
+import com.dawit.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudentDOAImpl implements StudentDAO{
+
+    // define field for entity manager
+
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+
+    @Autowired
+    public StudentDOAImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+}
