b20a98ce1e90c6611420124e3896efe7cd468ec4
Dawit Alem <dawitt.alem@gmail.com>
2025-08-12 13:15:23 -0400

    Update the database with JPA

---
+++ 03-spring-boot-hibernate-jpa-crud/02-updating-objects-with-JPA/src/main/java/com/dawit/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,18 @@
+package com.dawit.cruddemo.dao;
+
+import com.dawit.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+}
